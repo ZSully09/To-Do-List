756e6229ac96fbfbfa4279dbc1f309a4509f306b
Eli Kittross-Schnell <eliks92@gmail.com>
2019-11-17 21:01:17 +0000

    added basic request functionality for first movie and books apis

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,5 @@
+
+/*
 $(() => {
   $.ajax({
     method: "GET",
@@ -8,3 +10,23 @@ $(() => {
     }
   });;
 });
+*/
+
+
+const request = require('request')
+
+module.exports = {
+    /*
+    ** This method returns a promise
+    ** which gets resolved or rejected based
+    ** on the result from the API
+    */
+    apiRequest : function(url){
+        return new Promise((resolve, reject) => {
+            request(url, { json: true }, (err, res, body) => {
+              if (err) reject(err)
+              resolve(body)
+            });
+        })
+    }
+}
--- routes/widgets.js
+++ routes/widgets.js
@@ -7,6 +7,7 @@
 
 const express = require('express');
 const router  = express.Router();
+const help = require('../public/scripts/app')
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -23,5 +24,73 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+  router.post("/add", (req, res) => {
+    let movieData = {
+      // movieName,
+      // bookName,
+      // restaurantName,
+      // productName
+    }
+    let bookData = {
+
+    }
+    const name = req.body['api-search'];
+    // const moviePromise = help.apiRequest('http://www.omdbapi.com/?t=' + name + '&apikey=a370f17c')
+    // .then(response => {
+    //   // takes title from API call and saves it to temp object to compare results
+    //   if(response.Title) {
+    //   movieData.title = response['Title'];
+    //   movieData.year = response['Year'];
+    //   movieData.director = response['Director'];
+    //   movieData.poster = response['Poster'];
+    //   movieData.rating = response['Ratings'][1]['Value'];
+    //   movieData.boxOffice = response['BoxOffice'];
+    //   }
+    //   return movieData
+    // })
+    // .catch(error => {
+    //   res.send(error)
+    // })
+    // const bookPromise = help.apiRequest('https://www.googleapis.com/books/v1/volumes?q=' + name)
+    // .then(response => {
+    //   // takes title from API call and saves it to temp object to compare results
+    //   if(response.totalItems > 0) {
+
+    //     bookData.title = response.items[0]['volumeInfo']['title']
+    //     bookData.authors = response.items[0]['volumeInfo']['authors'][0]
+    //     bookData.description = response.items[0]['volumeInfo']['description']
+    //     bookData.thumbnail = response.items[0]['volumeInfo']['imageLinks']['smallThumbnail']
+    //     bookData.rating = response.items[0]['volumeInfo']['averageRating']
+    //   }
+    //   return bookData
+    // })
+    // .catch(error => {
+    //   res.send(error)
+    // })
+    const restaurantPromise = help.apiRequest('https://www.googleapis.com/books/v1/volumes?q=' + name)
+    .then(response => {
+      // takes title from API call and saves it to temp object to compare results
+      if(response.totalItems > 0) {
+
+        bookData.title = response.items[0]['volumeInfo']['title']
+        bookData.authors = response.items[0]['volumeInfo']['authors'][0]
+        bookData.description = response.items[0]['volumeInfo']['description']
+        bookData.thumbnail = response.items[0]['volumeInfo']['imageLinks']['smallThumbnail']
+        bookData.rating = response.items[0]['volumeInfo']['averageRating']
+      }
+      return bookData
+    })
+    .catch(error => {
+      res.send(error)
+    })
+    Promise.all([restaurantPromise])
+      .then(values =>{
+        console.log(values)
+        // meaty stuff happens here, compare respones, enter responses into database.
+        res.redirect('/')
+      })
+  });
+
   return router;
 };
+
