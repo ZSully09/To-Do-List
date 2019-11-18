6cf74bb37e4a13095199c31f0ddd7c3494fd22f7
ZSully09 <zsullivan93@gmail.com>
2019-11-18 22:06:11 +0000

    getUserById query for header

--- database.js
+++ database.js
@@ -1,19 +1,38 @@
 //database functions
 
-const addUser = function (user, db) {
-  return db.query(`
+const addUser = function(user, db) {
+  return db
+    .query(
+      `
   INSERT INTO users(email,password) VALUES($1,$2) RETURNING *;
-`, [`${user.email}`, `${user.password}`]).then(data => {
-    if (data.rows.length) {
-      return data.rows[0];
-    }
-  });
+`,
+      [`${user.email}`, `${user.password}`]
+    )
+    .then(data => {
+      if (data.rows.length) {
+        return data.rows[0];
+      }
+    });
 };
-const getUserByEmail = function (email, db) {
-  return db.query(`
-  SELECT * FROM users WHERE email=$1`, [`${email}`]);
+const getUserByEmail = function(email, db) {
+  return db.query(
+    `
+  SELECT * FROM users WHERE email=$1`,
+    [`${email}`]
+  );
 };
+
+// Get User by Id used to show a users email in the header.
+const getUserById = function(id, db) {
+  return db.query(
+    `
+  SELECT * FROM users WHERE id=$1`,
+    [`${id}`]
+  );
+};
+
 module.exports = {
   addUser,
-  getUserByEmail
+  getUserByEmail,
+  getUserById
 };
