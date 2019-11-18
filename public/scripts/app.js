5a27c506c3f8d80d83521c3bda9c9bf631bcb8aa
Eli Kittross-Schnell <eliks92@gmail.com>
2019-11-18 22:38:55 +0000

    deleted some helper functions to test problem with git

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -21,23 +21,6 @@ const client = yelp.client(process.env.YELP_KEY);
 
 module.exports = {
 
-  apiRequest : function(url){
-    return new Promise((resolve, reject) => {
-      request(url, { json: true }, (err, res, body) => {
-        if (err) reject(err)
-          resolve(body)
-        });
-    })
-  },
-  yelpRequest : function(name, location) {
-    const searchRequest = {
-      term: name,
-      location: location
-    };
-    return new Promise((resolve, reject) => {
-      resolve(client.search(searchRequest))
-    })
-  },
   compareResults : function(array, searchTerm) {
     let dbTitle = ""
 
