e00a0ccd38c24b2f8781f33d5633ecacacfdef83
ZSully09 <zsullivan93@gmail.com>
2019-11-18 20:10:13 +0000

    minor schema modifations for testing

---
+++ db/schema/00_users.sql
@@ -0,0 +1,11 @@
+-- Drop and recreate Users table (Example)
+
+DROP TABLE IF EXISTS users
+CASCADE;
+CREATE TABLE users
+(
+  id SERIAL PRIMARY KEY NOT NULL
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
+-- added item id
