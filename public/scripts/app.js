39f3bda08ed2b8b71338eb2e7ac7db420fb8fd2a
Eli Kittross-Schnell <eliks92@gmail.com>
2019-11-18 21:08:19 +0000

    added all api handling functionality. Successfully receiving responses from APIs when requested

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,20 +13,32 @@ $(() => {
 */
 
 
-const request = require('request')
+const request = require('request');
+const yelp = require('yelp-fusion');
+const client = yelp.client(process.env.YELP_KEY);
+
+
 
 module.exports = {
-    /*
-    ** This method returns a promise
-    ** which gets resolved or rejected based
-    ** on the result from the API
-    */
-    apiRequest : function(url){
-        return new Promise((resolve, reject) => {
-            request(url, { json: true }, (err, res, body) => {
-              if (err) reject(err)
-              resolve(body)
-            });
-        })
-    }
+
+  apiRequest : function(url){
+    return new Promise((resolve, reject) => {
+      request(url, { json: true }, (err, res, body) => {
+        if (err) reject(err)
+          resolve(body)
+        });
+    })
+  },
+  yelpRequest : function(name, location) {
+    const searchRequest = {
+      term: name,
+      location: location
+    };
+    return new Promise((resolve, reject) => {
+      resolve(client.search(searchRequest))
+    })
+  },
+
 }
+
+
