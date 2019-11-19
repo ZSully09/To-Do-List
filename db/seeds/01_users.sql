cbdb37692d72ac8eb2e94a94f1335e6b620b53e0
ZSully09 <zsullivan93@gmail.com>
2019-11-19 00:56:20 +0000

    changed name for users and items (reordering), and commented out items

--- db/seeds/01_users.sql
+++
@@ -1,9 +0,0 @@
--- Users table seeds here (Example)
-INSERT INTO users
-  (email, password)
-VALUES
-  ('Alice@gmail.com' , 'ab43uhD');
-INSERT INTO users
-  (email, password)
-VALUES
-  ('Paul@hotmail.com' , '0943gtS');
