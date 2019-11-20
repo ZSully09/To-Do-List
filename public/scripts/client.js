512a1b3d9a1bd63ed06cde64d7e5e9b559ca05c1
ZSully09 <zsullivan93@gmail.com>
2019-11-20 21:38:18 +0000

    current master commit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
-const createMovieItemElement = function (movies) {
-  const markup =
-    `<a href='http://localhost:8080/api/users/watch/${movies.item_id}'><section class="card movie">
+const createMovieItemElement = function(movies) {
+  const markup = `<a href='http://localhost:8080/api/users/watch/${movies.item_id}'><section class="card movie">
   <div class="img-div">
     <img
       class="thumbnail rounded-circle"
@@ -9,15 +8,13 @@ const createMovieItemElement = function (movies) {
       alt="friends"
     />
   </div>
-
   <p class="name">${movies.title}</p>
 </section></a>`;
 
   return markup;
 };
-const createBooksItemElement = function (books) {
-  const markup =
-    `<a href='http://localhost:8080/api/users/read/${books.item_id}'> <section class="card book">
+const createBooksItemElement = function(books) {
+  const markup = `<a href='http://localhost:8080/api/users/read/${books.item_id}'> <section class="card book">
   <div class="img-div">
     <img
       class="thumbnail rounded-circle"
@@ -25,17 +22,15 @@ const createBooksItemElement = function (books) {
       alt="friends"
     />
   </div>
-
   <p class="name">${books.title}</p>
 </section>
 </a>`;
 
   return markup;
 };
-const createRestaurantsItemElement = function (restaurant) {
+const createRestaurantsItemElement = function(restaurant) {
   console.log(restaurant);
-  const markup =
-    `<a href='http://localhost:8080/api/users/eat/${restaurant.item_id}'<section class="card restaurant">
+  const markup = `<a href='http://localhost:8080/api/users/eat/${restaurant.item_id}'<section class="card restaurant">
   <div class="img-div">
     <img
       class="thumbnail rounded-circle"
@@ -43,15 +38,13 @@ const createRestaurantsItemElement = function (restaurant) {
       alt="friends"
     />
   </div>
-
   <p class="name">${restaurant.name}</p>
 </section>`;
 
   return markup;
 };
-const createProductsItemElement = function (product) {
-  const markup =
-    `<a href='http://localhost:8080/api/users/buy/${product.item_id}' <section class="card product" >
+const createProductsItemElement = function(product) {
+  const markup = `<a href='http://localhost:8080/api/users/buy/${product.item_id}' <section class="card product" >
   <div class="img-div">
     <img
       class="thumbnail rounded-circle"
@@ -59,53 +52,12 @@ const createProductsItemElement = function (product) {
       alt="friends"
     />
   </div>
+  <p class="name">${product.name}</p>
+</section>`;
 
-//   return markup;
-// };
-
-<<<<<<< HEAD
-const createRestaurantsItemElement = function(restaurant) {
-  const $img = $('<img>').attr('src', restaurant.image);
-
-  const $divImg = $('<div>')
-    .addClass('img-div')
-    .append($img);
-
-  const $pName = $('<p>')
-    .addClass('name')
-    .text(restaurant.name);
-
-  const $divCard = $('<div>')
-    .addClass('card')
-    .append($divImg)
-    .append($pName);
-
-  return $divCard;
-};
-
-// const createRestaurantsItemElement = function(restaurant) {
-//   const markup = ` <section class="card">
-//   <div class="img-div">
-//     <img
-//       class="thumbnail rounded-circle"
-//       src=${restaurant.image}
-//       alt="friends"
-//     />
-//   </div>
-
-//   <p class="name">${restaurant.name}</p>
-// </section>`;
-
-//   return markup;
-// };
-
-const createProductsItemElement = function(product) {
-  const $img = $('<img>').attr('src', product.image);
-=======
   return markup;
 };
-const renderItems = function (obj) {
-
+const renderItems = function(obj) {
   for (const item in obj) {
     if (item === 'movies') {
       obj[item].forEach(element => {
@@ -119,7 +71,6 @@ const renderItems = function (obj) {
       obj[item].forEach(element => {
         $('.restaurants').append(createRestaurantsItemElement(element));
       });
-
     } else if (item === 'products') {
       obj[item].forEach(element => {
         $('.products').append(createProductsItemElement(element));
@@ -128,95 +79,18 @@ const renderItems = function (obj) {
   }
 };
 
-const loadItems = function () {
+const loadItems = function() {
   $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((data) => {
+    method: 'GET',
+    url: '/api/users'
+  }).done(data => {
     if (!data) {
-      alert("Error: not be able to fetch items");
+      alert('Error: not be able to fetch items');
     } else {
       renderItems(data);
     }
->>>>>>> 64d5499cfb30615f4f5cc91dce4125f7c49ac5c1
-
-  const $divImg = $('<div>')
-    .addClass('img-div')
-    .append($img);
-
-  const $pName = $('<p>')
-    .addClass('name')
-    .text(product.name);
-
-  const $divCard = $('<div>')
-    .addClass('card')
-    .append($divImg)
-    .append($pName);
-
-  return $divCard;
+  });
 };
-<<<<<<< HEAD
-
-// const createProductsItemElement = function(product) {
-//   const markup = ` <section class="card">
-//   <div class="img-div">
-//     <img
-//       class="thumbnail rounded-circle"
-//       src=${product.image}
-//       alt="friends"
-//     />
-//   </div>
-
-//   <p class="name">${product.name}</p>
-// </section>`;
-
-//   return markup;
-// };
 $(document).ready(function() {
-  const renderItems = function(obj) {
-    for (const item in obj) {
-      if (item === 'movies') {
-        obj[item].forEach(element => {
-          $('.movies').append(createMoviesItemElement(element));
-          console.log('Movie');
-        });
-      }
-      if (item === 'books') {
-        obj[item].forEach(element => {
-          $('.books').append(createBooksItemElement(element));
-          console.log('Books');
-        });
-      }
-      if (item === 'restaurants') {
-        obj[item].forEach(element => {
-          $('.restaurants').append(createRestaurantsItemElement(element));
-          console.log('Rest');
-        });
-      }
-      if (item === 'products') {
-        obj[item].forEach(element => {
-          $('.products').append(createProductsItemElement(element));
-          console.log('prod');
-        });
-      }
-    }
-  };
-
-  const loadItems = function() {
-    $.ajax({
-      method: 'GET',
-      url: '/api/users'
-    }).done(data => {
-      if (!data) {
-        alert('Error: not be able to fetch items');
-      } else {
-        // console.log(data);
-        renderItems(data);
-      }
-    });
-  };
-=======
-$(document).ready(function () {
->>>>>>> 64d5499cfb30615f4f5cc91dce4125f7c49ac5c1
   loadItems();
 });
