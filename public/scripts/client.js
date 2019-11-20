72b619893bae0e4a431dae58a91f8e73ece86c1b
ZSully09 <zsullivan93@gmail.com>
2019-11-20 20:40:07 +0000

    jquery category item element creation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,112 +1,185 @@
 /* eslint-disable no-undef */
-const createMovieItemElement = function (movies) {
+const createMoviesItemElement = function(movies) {
+  const $img = $('<img>').attr('src', movies.image);
 
-  const markup =
-    ` <section class="card">
-  <div class="img-div">
-    <img
-      class="thumbnail rounded-circle"
-      src=${movies.image}
-      alt="friends"
-    />
-  </div>
+  const $divImg = $('<div>')
+    .addClass('img-div')
+    .append($img);
 
-  <p class="name">${movies.title}</p>
-</section>`;
+  const $pName = $('<p>')
+    .addClass('name')
+    .text(movies.title);
 
-  return markup;
+  const $divCard = $('<div>')
+    .addClass('card')
+    .append($divImg)
+    .append($pName);
+
+  return $divCard;
 };
-const createBooksItemElement = function (books) {
+// const createMovieItemElement = function(movies) {
+//   const markup = ` <section class="card">
+//   <div class="img-div">
+//     <img
+//       class="thumbnail rounded-circle"
+//       src=${movies.image}
+//       alt="friends"
+//     />
+//   </div>
 
-  console.log(books);
-  const markup =
-    ` <section class="card">
-  <div class="img-div">
-    <img
-      class="thumbnail rounded-circle"
-      src=${books.image}
-      alt="friends"
-    />
-  </div>
+//   <p class="name">${movies.title}</p>
+// </section>`;
 
-  <p class="name">${books.title}</p>
-</section>`;
+//   return markup;
+// };
 
-  return markup;
+const createBooksItemElement = function(books) {
+  const $img = $('<img>').attr('src', books.image);
+
+  const $divImg = $('<div>')
+    .addClass('img-div')
+    .append($img);
+
+  const $pName = $('<p>')
+    .addClass('name')
+    .text(books.title);
+
+  const $divCard = $('<div>')
+    .addClass('card')
+    .append($divImg)
+    .append($pName);
+
+  return $divCard;
 };
-const createRestaurantsItemElement = function (restaurant) {
 
-  const markup =
-    ` <section class="card">
-  <div class="img-div">
-    <img
-      class="thumbnail rounded-circle"
-      src=${restaurant.image}
-      alt="friends"
-    />
-  </div>
+// const createBooksItemElement = function(books) {
+//   console.log(books);
+//   const markup = ` <section class="card">
+//   <div class="img-div">
+//     <img
+//       class="thumbnail rounded-circle"
+//       src=${books.image}
+//       alt="friends"
+//     />
+//   </div>
 
-  <p class="name">${restaurant.name}</p>
-</section>`;
+//   <p class="name">${books.title}</p>
+// </section>`;
 
-  return markup;
+//   return markup;
+// };
+
+const createRestaurantsItemElement = function(restaurant) {
+  const $img = $('<img>').attr('src', restaurant.image);
+
+  const $divImg = $('<div>')
+    .addClass('img-div')
+    .append($img);
+
+  const $pName = $('<p>')
+    .addClass('name')
+    .text(restaurant.name);
+
+  const $divCard = $('<div>')
+    .addClass('card')
+    .append($divImg)
+    .append($pName);
+
+  return $divCard;
 };
-const createProductsItemElement = function (product) {
 
+// const createRestaurantsItemElement = function(restaurant) {
+//   const markup = ` <section class="card">
+//   <div class="img-div">
+//     <img
+//       class="thumbnail rounded-circle"
+//       src=${restaurant.image}
+//       alt="friends"
+//     />
+//   </div>
 
-  const markup =
-    ` <section class="card">
-  <div class="img-div">
-    <img
-      class="thumbnail rounded-circle"
-      src=${product.image}
-      alt="friends"
-    />
-  </div>
+//   <p class="name">${restaurant.name}</p>
+// </section>`;
 
-  <p class="name">${product.name}</p>
-</section>`;
+//   return markup;
+// };
 
-  return markup;
+const createProductsItemElement = function(product) {
+  const $img = $('<img>').attr('src', product.image);
+
+  const $divImg = $('<div>')
+    .addClass('img-div')
+    .append($img);
+
+  const $pName = $('<p>')
+    .addClass('name')
+    .text(product.name);
+
+  const $divCard = $('<div>')
+    .addClass('card')
+    .append($divImg)
+    .append($pName);
+
+  return $divCard;
 };
-const renderMovieItems = function (obj) {
 
-  for (const item in obj) {
-    if (item === 'movies') {
-      obj[item].forEach(element => {
-        $('.movies').append(createMovieItemElement(element));
-      });
-    } else if (item === 'books') {
-      obj[item].forEach(element => {
-        $('.books').append(createBooksItemElement(element));
-      });
-    } else if (item === 'restaurants') {
-      obj[item].forEach(element => {
-        $('.restaurants').append(createRestaurantsItemElement(element));
-      });
+// const createProductsItemElement = function(product) {
+//   const markup = ` <section class="card">
+//   <div class="img-div">
+//     <img
+//       class="thumbnail rounded-circle"
+//       src=${product.image}
+//       alt="friends"
+//     />
+//   </div>
 
-    } else if (item === 'products') {
-      obj[item].forEach(element => {
-        $('.products').append(createProductsItemElement(element));
-      });
+//   <p class="name">${product.name}</p>
+// </section>`;
+
+//   return markup;
+// };
+$(document).ready(function() {
+  const renderItems = function(obj) {
+    for (const item in obj) {
+      if (item === 'movies') {
+        obj[item].forEach(element => {
+          $('.movies').append(createMoviesItemElement(element));
+          console.log('Movie');
+        });
+      }
+      if (item === 'books') {
+        obj[item].forEach(element => {
+          $('.books').append(createBooksItemElement(element));
+          console.log('Books');
+        });
+      }
+      if (item === 'restaurants') {
+        obj[item].forEach(element => {
+          $('.restaurants').append(createRestaurantsItemElement(element));
+          console.log('Rest');
+        });
+      }
+      if (item === 'products') {
+        obj[item].forEach(element => {
+          $('.products').append(createProductsItemElement(element));
+          console.log('prod');
+        });
+      }
     }
-  }
-};
+  };
 
-const loadMovieItems = function () {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((data) => {
-    if (!data) {
-      alert("Error: not be able to fetch tweets");
-    } else {
-      // console.log(data);
-      renderMovieItems(data);
-    }
-
-  });
-};
-$(document).ready(function () {
-  loadMovieItems();
+  const loadItems = function() {
+    $.ajax({
+      method: 'GET',
+      url: '/api/users'
+    }).done(data => {
+      if (!data) {
+        alert('Error: not be able to fetch items');
+      } else {
+        // console.log(data);
+        renderItems(data);
+      }
+    });
+  };
+  loadItems();
 });
